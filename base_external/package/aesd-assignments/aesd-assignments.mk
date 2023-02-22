f6f02fa709cfbffa07ea234e1a0b4bc1a7214cd3
Matt Schurmann <masc3796@colorado.edu>
2023-02-21 20:36:22 -0600

    test

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '463e23d2f40d1a6c992038c272bd4f53eb65b513'
+AESD_ASSIGNMENTS_VERSION = 'b44f1624e90e8d85b972935480c0d37f28b3d6bf'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
